5af31b973d2e5567c976ff6e6a7cad6e9d1c8a57
Hasan Bahadır NURAL <h.bahadirnural@gmail.com>
2024-08-17 17:57:24 +0300

    Unneccasry changes removed.

--- backend/src/api/endpoints/__init__.py
+++ backend/src/api/endpoints/__init__.py
@@ -1,2 +1,1 @@
 from .data import router as data_router
-from .security import router as security_router
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from api.endpoints import data_router, security_router
+from api.endpoints import data_router
 
 app = FastAPI()
 
 app.include_router(data_router, prefix="/data", tags=["Data"])
-app.include_router(security_router, prefix="/security", tags=["Security"])
 
 @app.get("/")
 async def root():
